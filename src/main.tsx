a92e42fad81d97f0ac6b196d02afde3b70a12eac
konthon <konthon@hotmail.co.th>
2024-11-19 21:53:08 +0700

    feat: add sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,27 @@
+import * as Sentry from '@sentry/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App'
 import { Provider } from './components/ui/provider'
+
+Sentry.init({
+  dsn: 'https://a6888aa63b2e0d5af6210bbf5179cd44@o4508325275172864.ingest.us.sentry.io/4508325410111488',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: [
+    'localhost',
+    /^https:\/\/cheaper\.konthon\.dev\/api/,
+  ],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
+import { sentryVitePlugin } from '@sentry/vite-plugin'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    sentryVitePlugin({
+      org: 'konthon',
+      project: 'cheaper',
+    }),
+  ],
+
+  build: {
+    sourcemap: true,
+  },
 })
